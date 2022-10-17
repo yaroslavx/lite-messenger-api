cb037e55ef2b215b95a0e5140f7b23dbd344bc57
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-10-17 17:22:17 +0300

    deploy

---
+++ index.js
@@ -0,0 +1,86 @@
+const express = require('express');
+let cors = require('cors');
+
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.use(cors());
+app.use(express.json());
+
+const rooms = new Map();
+
+app.get('/rooms/:id', (req, res) => {
+  const { id: roomId } = req.params;
+  const obj = rooms.has(roomId)
+    ? {
+        users: [...rooms.get(roomId).get('users').values()],
+        messages: [...rooms.get(roomId).get('messages').values()],
+      }
+    : { users: [], messages: [] };
+  res.json(obj);
+});
+
+app.post('/rooms', (req, res) => {
+  const { roomId, userName } = req.body;
+  if (!rooms.has(roomId)) {
+    rooms.set(
+      roomId,
+      new Map([
+        ['users', new Map()],
+        ['messages', []],
+      ])
+    );
+  }
+  res.send();
+});
+
+io.on('connection', (socket) => {
+  socket.on('ROOM:JOIN', ({ roomId, userName }) => {
+    socket.join(roomId);
+    rooms.get(roomId).get('users').set(socket.id, userName);
+    const users = [...rooms.get(roomId).get('users').values()];
+    console.log('join', roomId, rooms);
+    socket.to(roomId).emit('ROOM:SET_USERS', users);
+  });
+
+  socket.on('ROOM:NEW_MESSAGE', ({ roomId, userName, text, time }) => {
+    console.log(roomId, userName, text);
+    const obj = {
+      userName,
+      text,
+      time,
+    };
+    rooms.get(roomId).get('messages').push(obj);
+    socket.to(roomId).emit('ROOM:NEW_MESSAGE', obj);
+  });
+
+  socket.on('ROOM:LEFT', (room) => {
+    socket.leave(room);
+    rooms.forEach((value, roomId) => {
+      if (value.get('users').delete(socket.id)) {
+        const users = [...value.get('users').values()];
+        console.log('left', users);
+        socket.to(roomId).emit('ROOM:SET_USERS', users);
+      }
+    });
+  });
+
+  socket.on('disconnect', () => {
+    rooms.forEach((value, roomId) => {
+      if (value.get('users').delete(socket.id)) {
+        const users = [...value.get('users').values()];
+        socket.to(roomId).emit('ROOM:SET_USERS', users);
+      }
+    });
+  });
+
+  console.log('user connected', socket.id);
+});
+
+http.listen(7777, (err) => {
+  if (err) {
+    throw Error(err);
+  }
+  console.log('API started');
+});
