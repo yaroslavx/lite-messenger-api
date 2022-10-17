194e43134e16e23b586172e9832e600184c68785
Yaroslav Yakupov <yakupov.yaroslav@icloud.com>
2022-10-17 17:32:34 +0300

    fix deploy

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ io.on('connection', (socket) => {
   console.log('user connected', socket.id);
 });
 
-http.listen(7777, (err) => {
+http.listen(process.env.PORT || 5000, (err) => {
   if (err) {
     throw Error(err);
   }
